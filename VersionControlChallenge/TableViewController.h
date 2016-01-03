ce07fe691002585492839bc8fb5ab8ac5dbfa1c6
Jong <jong@Jongs-MacBook-Pro.local>
2016-01-03 11:47:11 +0800

    update to add table view controller

--- VersionControlChallenge/TableViewController.h
+++ VersionControlChallenge/TableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  TableViewController.h
 //  VersionControlChallenge
 //
 //  Created by Jong on 1/3/16.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface TableViewController : UITableViewController
 
 @end
-
