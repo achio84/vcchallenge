cb18a995d28600c0b384661a56bc98ef5ec5e793
Jong <jong@jongs-macbook-pro.local>
2016-01-03 11:17:05 +0800

    Initial Commit

---
+++ VersionControlChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VersionControlChallenge
+//
+//  Created by Jong on 1/3/16.
+//  Copyright © 2016 Jong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VersionControlChallenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VersionControlChallenge
+//
+//  Created by Jong on 1/3/16.
+//  Copyright © 2016 Jong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
